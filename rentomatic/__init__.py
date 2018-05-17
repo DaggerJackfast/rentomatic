0ea500effc334f7b93f60f37607550213b5ec465
Aibek <aibek.abdykasymov@gmail.com>
2018-05-18 00:25:33 +0600

    initial commit

---
+++ rentomatic/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for rentomatic."""
+
+__author__ = """Aibek Abdykasymov"""
+__email__ = 'aibek.abdykasymov@gmail.com'
+__version__ = '0.1.0'
---
+++ tests/serializers/test_storageroom_serializer.py
@@ -0,0 +1,30 @@
+import datetime
+import pytest
+import json
+
+from rentomatic.serializers import storageroom_serializer as srs
+from rentomatic.domain.storageroom import StorageRoom
+
+
+def test_serialize_domain_storageroom():
+    room = StorageRoom('f853578c-fc0f-4e65-81b8-566c5dffa35a',
+                       size=200,
+                       price=10,
+                       longitude='-0.09998975',
+                       latitude='51.75436293'
+                       )
+    expected_json = """
+        {
+            "code": "f853578c-fc0f-4e65-81b8-566c5dffa35",
+            "size": 200,
+            "price": 10,
+            "longitude": -0.09998975,
+            "size": 51.75436293
+        }
+    """
+    assert json.loads(json.dumps(room, cls=srs.StorageRoomEncoder)) == json.loads(expected_json)
+
+
+def test_serialize_domain_storageroom_wrong_type():
+    with pytest.raises(TypeError):
+        json.dumps(datetime.datetime.now(), cls=srs.StorageRoomEncoder)
