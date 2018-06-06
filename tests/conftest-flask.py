e314d0f1687a126e8e77bab21368533db429d992
Aibek <aibek.abdykasymov@gmail.com>
2018-06-06 22:08:30 +0600

    delete flask from project

--- tests/conftest-flask.py
+++
@@ -1,9 +0,0 @@
-# import pytest
-#
-# from rentomatic.flask_app import create_app
-# from rentomatic.settings import TestConfig
-#
-#
-# @pytest.yield_fixture(scope='function')
-# def app():
-#     return create_app(TestConfig)
--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,6 @@ import pytest
 from falcon import testing
 from rentomatic.app import create_app
 
-
 # TODO : add test configuration
 @pytest.yield_fixture()
 def client():
