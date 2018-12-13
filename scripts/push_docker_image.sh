a7423d0dd2ac1a7be0d28bb93929126a5cfa75ac
Aibek <aibek.abdykasymov@gmail.com>
2018-12-13 14:01:34 +0600

    fix setting docker image tag

--- scripts/push_docker_image.sh
+++ scripts/push_docker_image.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-DOCKER_IMAGE_TAG = $DOCKER_USERNAME/rentomatic:beta.$TRAVIS_BUILD_NUMBER
+DOCKER_IMAGE_TAG=$DOCKER_USERNAME/rentomatic:beta.$TRAVIS_BUILD_NUMBER
 docker build -t $DOCKER_IMAGE_TAG .
-docker push DOCKER_IMAGE_TAG
+docker push $DOCKER_IMAGE_TAG
